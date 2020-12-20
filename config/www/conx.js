c52def100e0f930cd4bb083d5a638f6aa2faeb80
zarlior <zarlior@gmail.com>
2020-12-20 20:19:12 +0200

    round

--- config/www/conx.js
+++ config/www/conx.js
@@ -820,13 +820,13 @@ var conx;
     (function (cards) {
         class Swatch extends cards.HACard {
             create() {
-                var _a, _b;
+                var _a, _b, _c;
                 super.create();
                 let html = ``;
-                let size = ((_a = this.config) === null || _a === void 0 ? void 0 : _a.size) || 50, space = ((_b = this.config) === null || _b === void 0 ? void 0 : _b.space) || 2, clr;
+                let size = ((_a = this.config) === null || _a === void 0 ? void 0 : _a.size) || 50, space = ((_b = this.config) === null || _b === void 0 ? void 0 : _b.space) || 2, radius = ((_c = this.config) === null || _c === void 0 ? void 0 : _c.radius) || 4, clr;
                 for (let i = 0; i < this.config.colors.length; ++i) {
                     clr = this.config.colors[i];
-                    html += `<button id="clr${i}" style="width:${size}px; height:${size}px; background-color:${clr}; margin:${space}px;"></button>`;
+                    html += `<button id="clr${i}" style="width:${size}px; height:${size}px; background-color:${clr}; margin:${space}px; border-radius:${radius}px"></button>`;
                 }
                 this.innerHTML = `<div id="main">${html}</div>`;
                 this.root = conx.glo.findChild(this, "main");
